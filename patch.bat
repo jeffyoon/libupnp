3c6dfc55e6cb63d85510ae1d1b2e3f3970abc3d7
atraczyk <andreastraczyk@gmail.com>
2016-06-03 11:47:28 -0400

    patching

--- patch.bat
+++ patch.bat
@@ -2,7 +2,7 @@
 
 SETLOCAL EnableDelayedExpansion
 
-for %%i in (..src\upnp\*) do (
+for %%i in (..\src\upnp\*) do (
     echo applying patch: %%~nxi
     git apply --reject --ignore-space-change --ignore-whitespace --whitespace=nowarn %%i
 )
